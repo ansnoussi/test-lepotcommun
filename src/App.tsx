b9b579f862ca9f78e065053385a20232836e8c06
Anis Snoussi <snoussi.anis@insat.u-carthage.tn>
2021-11-15 21:32:36 +0100

    loading indicator

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,35 @@
-import React, { useEffect } from 'react';
-import './styles/global.scss';
-import di from './di';
-import PageHeader from '@components/pageHeader';
+import React, { useEffect, useState, useCallback } from "react";
+import "./styles/global.scss";
+import di from "./di";
+import PageHeader from "@components/pageHeader";
 
 function App() {
+  const [isLoading, setIsLoading] = useState<Boolean>(false);
+  const [partnersList, setPartnersList] = useState<any>(null);
+  const [categoriesList, setCategoriesList] = useState<any>(null);
+
+  const fetchFromAPI = useCallback(async () => {
+    setIsLoading(true);
+
+    const [partners, categories] = await Promise.all([
+      di.partner.getPartners(),
+      di.partnerCategory.getPartnersCategries(),
+    ]);
+
+    setIsLoading(false);
+    setPartnersList(partners);
+    setCategoriesList(categories);
+  }, []);
 
   useEffect(() => {
-   di.partner.getPartners().then(data => console.log(data))
-   di.partnerCategory.getPartnersCategries().then(data => console.log(data))
-  }, [])
+    fetchFromAPI();
+  }, [fetchFromAPI]);
 
   return (
     <div className="App">
       <PageHeader />
+      <p>Hello</p>
+      {isLoading ? <img src={"/images/loading.svg"} /> : null}
     </div>
   );
 }
